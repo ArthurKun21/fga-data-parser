71810762fd920ac7fc0bb0dd5a640ef12a410c19
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-02-21 11:46:34 +0800

    feat: implement data downloading and reading utilities for servant and mystic code

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from master import mystic_code
+from pathlib import Path
+from utils import download_data, read_data
+
+CWD = Path(__file__).parent
+
+
+def servant_data():
+    url = "https://api.atlasacademy.io/export/JP/nice_servant_lang_en.json"
+    file_path = CWD / "nice_servant_lang_en.json"
+    download_data(file_path, url)
+    return read_data(file_path)
+
+
+def mystic_code_data():
+    url = "https://api.atlasacademy.io/export/JP/nice_mystic_code_lang_en.json"
+    file_path = CWD / "nice_mystic_code_lang_en.json"
+    download_data(file_path, url)
+    return read_data(file_path)
 
 
 def main():
-    mystic_code.run()
+    servants = servant_data()
+    mystic_codes = mystic_code_data()
+    print(servants)
+    print(mystic_codes)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -4,15 +4,10 @@ from pathlib import Path
 import httpx
 import orjson
 
-CWD = Path(__file__).parent
 
-
-def download_data():
-    file_path = CWD / "nice_mystic_code_lang_en.json"
+def download_data(file_path: Path, url: str):
     if file_path.exists() and file_path.stat().st_size > 10_000:
         return
-
-    url = "https://api.atlasacademy.io/export/JP/nice_mystic_code_lang_en.json"
 
     retry = 3
 
@@ -29,17 +24,13 @@ def download_data():
             continue
 
 
-def read_data() -> list[dict]:
+def read_data(
+    file_path: Path,
+) -> list[dict]:
     try:
-        with open(CWD / "nice_mystic_code_lang_en.json", "r", encoding="utf-8") as f:
+        with open(file_path, "r", encoding="utf-8") as f:
             data = orjson.loads(f.read())
         return data
     except FileNotFoundError:
         print("File not found.")
         return []
-
-
-def run():
-    download_data()
-    data = read_data()
-    print(data)
